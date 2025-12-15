3296929b92210aa96601a76c4f6165120e36f0bc
mohamed-attia <m.attia@outlook.sa>
2025-12-15 21:03:59 +0300

    add teams

--- teamData.ts
+++ teamData.ts
@@ -3,58 +3,58 @@ import { TeamMember } from './types';
 export const teamMembers: TeamMember[] = [
   {
     "id": 1,
-    "name": "سارة أحمد",
-    "role": "Senior Frontend Engineer",
-    "photoUrl": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Amr Rashed",
+    "role": "Engineering Leader",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4D03AQEUndZ_MGyCHA/profile-displayphoto-scale_200_200/B4DZomCpnUKQAY-/0/1761574821421?e=1767225600&v=beta&t=-Fr6BxIdmVlgXcaPbyyWXhlbrzv-7urnbs8J_v0GHY8",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/amr-a-rashed/"
   },
   {
     "id": 2,
-    "name": "محمد علي",
-    "role": "Backend Team Lead",
-    "photoUrl": "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Sanaa Afifi",
+    "role": "Lead Software Engineer",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/C5603AQHGA7VzeY5_6w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1526630456366?e=1767225600&v=beta&t=UwUK7Un2YhovVC3fhZmfMPVHyYcNzJ1cfFqOtS790Zw",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/sanaaafifi/"
   },
   {
     "id": 3,
-    "name": "خالد عمر",
-    "role": "Product Manager",
-    "photoUrl": "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Anas Rabei",
+    "role": "Java Team Lead",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4D03AQHoVBt4KID5sQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1683245101249?e=1767225600&v=beta&t=Ksr1M6ZRXHyLwDyRbIorb1K60g4auzBIPG3LvXGRn_w",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/anas-rabei/"
   },
   {
     "id": 4,
-    "name": "لينا خالد",
-    "role": "UX Designer",
-    "photoUrl": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Moamen Abdulraouf",
+    "role": "Expert frontend engineer | Angular | React | Next.js | NX ",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4E03AQEMWUfo3sOnJA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1665304055440?e=1767225600&v=beta&t=2-cKn0edf88fGTaEtpTfcPkHt2x--qhLf1GtR8jq_rI",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/moamen/"
   },
   {
     "id": 6,
-    "name": "نورة فهد",
-    "role": "Mobile Developer",
-    "photoUrl": "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Ahmad Ragab",
+    "role": "Senior iOS Engineer | Swift, SwiftUI, Combine | Mobile Architecture & Team Mentorship",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4D03AQE6DMUlhzWPRA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1725975082426?e=1767225600&v=beta&t=-_A-OyjGjLIb3c8KiJ23l7lGFpQodz3r_uMj6DJNQoU",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/ahmad-ragab-88b27836/"
   },
   {
     "id": 7,
-    "name": "عمر يوسف",
-    "role": "Senior UX Designer",
-    "photoUrl": "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Muhammad Helmi",
+    "role": "Senior Android Engineer | Built STC Bank & STC Pay for 12M+ users | Kotlin Multiplatform | Clean Architecture | Jetpack Composer",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4D03AQH1KA3KIxANTw/profile-displayphoto-scale_200_200/B4DZqcRJ7hHsAY-/0/1763558335929?e=1767225600&v=beta&t=0tKOhPZ2QkIe1-36zaWVUmqeIUSlanFMYLTltYcOED0",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/developermohamedhelmy/"
   },
   {
     "id": 8,
-    "name": "ريما عبدالله",
-    "role": "QA Engineer",
-    "photoUrl": "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "name": "Ahmed Saeed",
+    "role": "Senior iOS Developer at STCBank | Expert in Mobile App Development",
+    "photoUrl": "https://media.licdn.com/dms/image/v2/D4D03AQG7aNfviv9hpg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1722794057634?e=1767225600&v=beta&t=ZpfvbnQWUN39uurYto0vp13gxPedQ7tdxJSJxsY_GsM",
     "rating": 5.0,
-    "linkedinUrl": "https://linkedin.com"
+    "linkedinUrl": "https://www.linkedin.com/in/ihendi/"
   }
 ];
